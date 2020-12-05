fe84790948b66892171a8fb52566b07b7e50d1e1
Nicolas Bigaouette <nbigaouette@gmail.com>
2020-12-04 22:39:43 -0500

    Solve part 2

--- day04/src/initial.rs
+++ day04/src/initial.rs
@@ -23,7 +23,13 @@ impl<'a> AoC<'a> for Day04Initial<'a> {
             .count()
     }
 
-    // fn solution_part2(&self) -> Self::SolutionPart2 {
+    fn solution_part2(&self) -> Self::SolutionPart2 {
+        parse_input(self.input)
+            .filter_map(|passport_builder| passport_builder.build_part2())
+            .count()
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct ValidPassportCommon<'a> {
     /// Height
@@ -94,7 +100,178 @@ impl<'a> PassportBuilder<'a> {
     pub fn is_valid_part1(&self) -> bool {
         self.clone().build_part1().is_some()
     }
+
+    pub fn build_part2(self) -> Option<ValidPassport<'a>> {
+        match (
+            self.byr, self.iyr, self.eyr, self.hgt, self.hcl, self.ecl, self.pid,
+        ) {
+            (Some(byr), Some(iyr), Some(eyr), Some(hgt), Some(hcl), Some(ecl), Some(pid)) => {
+                // Check each field for their own validity
+                if hgt.is_valid()
+                    && iyr.is_valid()
+                    && eyr.is_valid()
+                    && hcl.is_valid()
+                    && ecl.is_valid()
+                    && pid.is_valid()
+                    && byr.is_valid()
+                {
+                    let common = ValidPassportCommon {
+                        hgt,
+                        iyr,
+                        eyr,
+                        hcl,
+                        ecl,
+                        pid,
+                        byr,
+                    };
+
+                    match self.cid {
+                        Some(cid) => {
+                            Some(ValidPassport::ValidPassportWithCid(ValidPassportWithCid {
+                                cid,
+                                common,
+                            }))
+                        }
+                        None => Some(ValidPassport::ValidPassportWithoutCid(
+                            ValidPassportWithoutCid { common },
+                        )),
+                    }
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }
+    }
+
+    pub fn is_valid_part2(&self) -> bool {
+        self.clone().build_part2().is_some()
+    }
 }
+
+mod part2 {
+    use super::*;
+
+    impl<'a> BirthYear<'a> {
+        pub fn is_valid(&self) -> bool {
+            // four digits; at least 1920 and at most 2002.
+            let value = self.0;
+            match value.chars().count() {
+                4 => match value.parse::<i16>() {
+                    Ok(parsed) => 1920 <= parsed && parsed <= 2002,
+                    Err(err) => {
+                        // log::info!("Failed to parse {:?} as integer", value);
+                        false
+                    }
+                },
+                _ => false,
+            }
+        }
+    }
+
+    impl<'a> IssueYear<'a> {
+        pub fn is_valid(&self) -> bool {
+            // four digits; at least 2010 and at most 2020.
+            let value = self.0;
+            match value.chars().count() {
+                4 => match value.parse::<i16>() {
+                    Ok(parsed) => 2010 <= parsed && parsed <= 2020,
+                    Err(err) => {
+                        // log::info!("Failed to parse {:?} as integer", value);
+                        false
+                    }
+                },
+                _ => false,
+            }
+        }
+    }
+
+    impl<'a> ExpirationYear<'a> {
+        pub fn is_valid(&self) -> bool {
+            // four digits; at least 2020 and at most 2030.
+            let value = self.0;
+            match value.chars().count() {
+                4 => match value.parse::<i16>() {
+                    Ok(parsed) => 2020 <= parsed && parsed <= 2030,
+                    Err(err) => {
+                        // log::info!("Failed to parse {:?} as integer", value);
+                        false
+                    }
+                },
+                _ => false,
+            }
+        }
+    }
+
+    impl<'a> Height<'a> {
+        pub fn is_valid(&self) -> bool {
+            // a number followed by either cm or in:
+            // If cm, the number must be at least 150 and at most 193.
+            // If in, the number must be at least 59 and at most 76.
+            let value = self.0;
+            let height = value.trim_end_matches("cm").trim_end_matches("in");
+            match height.parse::<i16>() {
+                Ok(height) => {
+                    if value.ends_with("cm") {
+                        150 <= height && height <= 193
+                    } else if value.ends_with("in") {
+                        59 <= height && height <= 76
+                    } else {
+                        // log::info!("Failed to get unit for height {:?}", value);
+                        false
+                    }
+                }
+                Err(err) => {
+                    // log::info!("Failed to parse height {:?} as integer", height);
+                    false
+                }
+            }
+        }
+    }
+
+    impl<'a> HairColor<'a> {
+        pub fn is_valid(&self) -> bool {
+            // a # followed by exactly six characters 0-9 or a-f.
+            let value = self.0;
+            match value.chars().count() {
+                7 => {
+                    let mut color_iter = value.chars();
+                    match color_iter.next().expect("a character") {
+                        '#' => color_iter.all(|c| matches!(c, ('0'..='9') | ('a'..='f'))),
+                        _ => false,
+                    }
+                }
+                _ => false,
+            }
+        }
+    }
+
+    impl<'a> EyeColor<'a> {
+        pub fn is_valid(&self) -> bool {
+            // exactly one of: amb blu brn gry grn hzl oth.
+            let value = self.0;
+
+            matches!(value, "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth")
+        }
+    }
+
+    impl<'a> PassportId<'a> {
+        pub fn is_valid(&self) -> bool {
+            // a nine-digit number, including leading zeroes.
+            let value = self.0;
+
+            match value.chars().count() {
+                9 => value.chars().all(|c| matches!(c, '0'..='9')),
+                _ => false,
+            }
+        }
+    }
+
+    // impl<'a> CountryId<'a> {
+    //     pub fn is_valid(&self) -> bool {
+    //         // ignored, missing or not.
+    //         true
+    //     }
     // }
 }
 
@@ -189,9 +366,7 @@ mod tests {
             fn solution() {
                 init_logger();
 
-                unimplemented!();
-
-                let expected = 0;
+                let expected = 123;
                 let to_check = Day04Initial::new(PUZZLE_INPUT).solution_part2();
 
                 assert_eq!(to_check, expected);
@@ -200,19 +375,89 @@ mod tests {
 
         mod given {
             use super::super::super::Day04Initial;
-            use crate::{tests::init_logger, AoC};
+            use crate::{
+                parse_input, tests::init_logger, AoC, BirthYear, EyeColor, HairColor, Height,
+                PassportId,
+            };
 
             #[test]
-            fn ex01() {
+            fn birth_year() {
                 init_logger();
 
-                unimplemented!();
+                assert!(BirthYear("2002").is_valid());
+                assert!(!BirthYear("2003").is_valid());
+            }
 
-                let expected = 0;
-                let input = "";
-                let to_check = Day04Initial::new(input).solution_part2();
+            #[test]
+            fn height() {
+                init_logger();
 
-                assert_eq!(to_check, expected);
+                assert!(Height("60in").is_valid());
+                assert!(Height("190cm").is_valid());
+                assert!(!Height("190in").is_valid());
+                assert!(!Height("190").is_valid());
+            }
+
+            #[test]
+            fn hair_color() {
+                init_logger();
+
+                assert!(HairColor("#123abc").is_valid());
+                assert!(!HairColor("#123abz").is_valid());
+                assert!(!HairColor("123abc").is_valid());
+            }
+
+            #[test]
+            fn eye_color() {
+                init_logger();
+
+                assert!(EyeColor("brn").is_valid());
+                assert!(!EyeColor("wat").is_valid());
+            }
+
+            #[test]
+            fn passport_id() {
+                init_logger();
+
+                assert!(PassportId("000000001").is_valid());
+                assert!(!PassportId("0123456789").is_valid());
+            }
+
+            #[test]
+            fn invalid_passports() {
+                let passports = "eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007";
+
+                assert!(parse_input(passports).all(|passport| !passport.is_valid_part2()));
+            }
+
+            #[test]
+            fn valid_passports() {
+                let passports = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719";
+
+                assert!(parse_input(passports).all(|passport| passport.is_valid_part2()));
             }
         }
 
