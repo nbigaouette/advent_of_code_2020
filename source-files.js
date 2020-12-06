ebd6ded101be964c126e18bf7b5483157c99ff84
nbigaouette <nbigaouette@users.noreply.github.com>
2020-12-06 15:59:40 +0000

    deploy: 8b8a5ca370379b0691afc7b4e6f9307a91c63f68

--- source-files.js
+++ source-files.js
@@ -4,5 +4,5 @@ sourcesIndex["day02"] = {"name":"","files":["initial.rs","lib.rs"]};
 sourcesIndex["day03"] = {"name":"","files":["initial.rs","lib.rs"]};
 sourcesIndex["day04"] = {"name":"","files":["initial.rs","lib.rs"]};
 sourcesIndex["day05"] = {"name":"","files":["initial.rs","lib.rs"]};
-sourcesIndex["day06"] = {"name":"","files":["initial.rs","lib.rs"]};
+sourcesIndex["day06"] = {"name":"","files":["bits.rs","initial.rs","lib.rs"]};
 createSourceSidebar();
