846d3a66d70b98522bafa2ad5492160c55b716da
nbigaouette <nbigaouette@users.noreply.github.com>
2020-12-06 15:23:54 +0000

    deploy: e8c194cbebccc9d7de83e009171dc1937d79b105

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["day01"] = [{"text":"impl Deref for Day01Entry","synthetic":false,"types":[]}];
+implementors["day06"] = [{"text":"impl Deref for Day06Entry","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -4,4 +4,5 @@ sourcesIndex["day02"] = {"name":"","files":["initial.rs","lib.rs"]};
 sourcesIndex["day03"] = {"name":"","files":["initial.rs","lib.rs"]};
 sourcesIndex["day04"] = {"name":"","files":["initial.rs","lib.rs"]};
 sourcesIndex["day05"] = {"name":"","files":["initial.rs","lib.rs"]};
+sourcesIndex["day06"] = {"name":"","files":["initial.rs","lib.rs"]};
 createSourceSidebar();
