1191b851ea6ed429aaba952f1081b985ce3b732e
Nicolas Bigaouette <nbigaouette@gmail.com>
2020-12-08 11:10:48 -0500

    Solution part 2

--- day08/src/initial.rs
+++ day08/src/initial.rs
@@ -40,8 +40,74 @@ impl<'a> AoC<'a> for Day08Initial<'a> {
             }
         }
 
-    // fn solution_part2(&self) -> Self::SolutionPart2 {
-    // }
+        accumulator
+    }
+
+    fn solution_part2(&self) -> Self::SolutionPart2 {
+        let mut instructions: Vec<Day08Entry> = parse_input(self.input).collect();
+
+        let instructions_to_replace: Vec<(usize, Instruction)> = instructions
+            .iter()
+            .enumerate()
+            .filter_map(|(idx, instruction)| match instruction.0 {
+                Instruction::Jump(argument) => Some((idx, Instruction::NoOp(argument))),
+                Instruction::NoOp(argument) => Some((idx, Instruction::Jump(argument))),
+                _ => None,
+            })
+            .collect();
+
+        let max_steps = instructions.len();
+
+        for (idx, mut to_replace) in instructions_to_replace {
+            log::info!("Replacing index {} with {:?}", idx, to_replace);
+
+            std::mem::swap(&mut to_replace, &mut instructions[idx].0);
+
+            match part2_run_instructions(&instructions, max_steps) {
+                None => {
+                    // Reset and continue
+                    std::mem::swap(&mut to_replace, &mut instructions[idx].0);
+                }
+                Some(answer) => {
+                    // We found a solution!
+                    return answer;
+                }
+            }
+        }
+
+        unreachable!()
+    }
+}
+
+fn part2_run_instructions(instructions: &[Day08Entry], max_instructions: usize) -> Option<isize> {
+    let mut instruction_idx: isize = 0;
+    let mut accumulator = 0;
+    let mut instruction_count = 0;
+
+    loop {
+        if instruction_count == max_instructions {
+            break None;
+        } else {
+            instruction_count += 1;
+
+            if instruction_idx == instructions.len() as isize {
+                break Some(accumulator);
+            } else {
+                match instructions[instruction_idx as usize].0 {
+                    Instruction::Accumulator(argument) => {
+                        accumulator += argument;
+                        instruction_idx += 1;
+                    }
+                    Instruction::Jump(argument) => {
+                        instruction_idx += argument;
+                    }
+                    Instruction::NoOp(_argument) => {
+                        instruction_idx += 1;
+                    }
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -103,9 +169,7 @@ mod tests {
             fn solution() {
                 init_logger();
 
-                unimplemented!();
-
-                let expected = 0;
+                let expected = 1643;
                 let to_check = Day08Initial::new(PUZZLE_INPUT).solution_part2();
 
                 assert_eq!(to_check, expected);
@@ -120,10 +184,16 @@ mod tests {
             fn ex01() {
                 init_logger();
 
-                unimplemented!();
-
-                let expected = 0;
-                let input = "";
+                let expected = 8;
+                let input = "nop +0
+                                acc +1
+                                jmp +4
+                                acc +3
+                                jmp -3
+                                acc -99
+                                acc +1
+                                jmp -4
+                                acc +6";
                 let to_check = Day08Initial::new(input).solution_part2();
 
                 assert_eq!(to_check, expected);
